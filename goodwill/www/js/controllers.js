e2afccf2b72938da99c51dfc1c3b5496bf457eae
cb818575 <cboucher@udel.edu>
2015-10-30 23:52:00 -0400

    goodwill banner added and some fb login

--- goodwill/www/js/controllers.js
+++ goodwill/www/js/controllers.js
@@ -40,4 +40,18 @@ angular.module('starter.controllers', ['starter.services', 'ngOpenFB'])
             });
         });
     }
+})
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, ngFB) {
+    $scope.fbLogin = function () {
+    ngFB.login({scope: 'email,read_stream,publish_actions'}).then(
+        function (response) {
+            if (response.status === 'connected') {
+                console.log('Facebook login succeeded');
+                $scope.closeLogin();
+            } else {
+                alert('Facebook login failed');
+            }
+        });
+    };
 });
